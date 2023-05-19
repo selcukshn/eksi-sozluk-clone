eef43823115507f06cf3590543255b8317ca2ebd
selcuksahin <muselcuksahin@gmail.com>
2023-05-19 23:09:47 +0300

    extension class for blazor services registration

--- src/Client/Blazor/Program.cs
+++ src/Client/Blazor/Program.cs
@@ -2,9 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazor;
 using Common.Helpers;
-using Blazor.Services.Request.Base;
-using Blazor.Services.Request.Entry;
-using Blazor.Services.Request.EntryComment;
+using Blazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,7 +12,5 @@ builder.Services.AddTransient(sp => new HttpClient
 {
     BaseAddress = new Uri(WebConstants.ApiHttpsAddress),
 });
-builder.Services.AddTransient<IRequestService, RequestService>();
-builder.Services.AddTransient<IEntryRequestService, EntryRequestService>();
-builder.Services.AddTransient<IEntryCommentRequestService, EntryCommentRequestService>();
+builder.Services.AddServicesDependencies();
 await builder.Build().RunAsync();
---
+++ src/Client/Blazor/Services/RequestRegistration.cs
@@ -0,0 +1,17 @@
+using Blazor.Services.Request.Base;
+using Blazor.Services.Request.Entry;
+using Blazor.Services.Request.EntryComment;
+
+namespace Blazor.Services
+{
+    public static class RequestRegistration
+    {
+        public static IServiceCollection AddServicesDependencies(this IServiceCollection service)
+        {
+            service.AddTransient<IRequestService, RequestService>();
+            service.AddTransient<IEntryRequestService, EntryRequestService>();
+            service.AddTransient<IEntryCommentRequestService, EntryCommentRequestService>();
+            return service;
+        }
+    }
+}
